70e0265774222c9fa46c34e4c68a65d3b142edaa
CampbellChristopher <Camp7325@gmail.com>
2016-04-25 11:34:11 -0700

    Slight cleanups

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -6,19 +6,22 @@ import java.util.*;
 import static j2html.TagCreator.*;
 import static spark.Spark.*;
 
+/**
+ * Chat class handles sending chat messages as Html constructs through the chat WebSocket
+ */
 @WebSocket
 public class Chat {
-
     static Map<Session, String> userUsernameMap = new HashMap<>();
     static int nextUserNumber = 1; //Assign to username for next connecting user
-
     public static void main(String[] args) {
         staticFileLocation("public"); //index.html is served at localhost:4567 (default port)
         webSocket("/chat", ChatWebSocketHandler.class);
         init();
     }
 
-    //Sends a message from one user to all users, along with a list of current usernames
+    /**
+     * Sends a message from one user to all users, along with a list of current usernames
+     */
     public static void broadcastMessage(String sender, String message) {
         userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
             try {
@@ -32,7 +35,9 @@ public class Chat {
         });
     }
 
-    //Builds a HTML element with a sender-name, a message, and a timestamp,
+    /**
+     * Builds a HTML element with a sender-name, a message, and a timestamp.
+     */
     private static String createHtmlMessageFromSender(String sender, String message) {
         return article().with(
                 b(sender + " says:"),
--- src/main/java/ChatWebSocketHandler.java
+++ src/main/java/ChatWebSocketHandler.java
@@ -2,11 +2,19 @@
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 
+/**
+ * Contains WebSocket functions for conecting, messaging, and closing.
+ */
 @WebSocket
 public class ChatWebSocketHandler {
 
     private String sender, msg;
 
+    /**
+     * On connect creates and assigns username and outputs that user joined
+     * @param user - individual entering the room
+     * @throws Exception
+     */
     @OnWebSocketConnect
     public void onConnect(Session user) throws Exception {
         String username = "User" + Chat.nextUserNumber++;
@@ -14,6 +22,12 @@ public class ChatWebSocketHandler {
         Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
     }
 
+    /**
+     * On close, tells room that user has left room then removes them from userlist
+     * @param user - individual leaving the room
+     * @param statusCode
+     * @param reason
+     */
     @OnWebSocketClose
     public void onClose(Session user, int statusCode, String reason) {
         String username = Chat.userUsernameMap.get(user);
@@ -21,6 +35,11 @@ public class ChatWebSocketHandler {
         Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
     }
 
+    /**
+     * On message, use chat functions so send message to all users in room
+     * @param user -  individual sending the message
+     * @param message - string to be sent to other users
+     */
     @OnWebSocketMessage
     public void onMessage(Session user, String message) {
         Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
